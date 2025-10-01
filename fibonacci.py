aa47dcb37327d0f18336a3887c50b09ab31b07b2
Muhammad Umar Jafri <mjafri01@uoguelph.ca>
2025-10-01 12:05:30 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,9 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+  input ("how many terms of fibonacci sequence do you want?"
+         if input < 0 
+         print "invalid number select a number greater then 0"
+print return
+}
